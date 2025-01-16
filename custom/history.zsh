a9ae8ea61aa21344815e4782c77899fb7d5e8a01
Denis Telnov <denis.telnov@gmail.com>
2025-01-17 02:17:45 +0300

    Fix HISTORY_IGNORE option

--- custom/history.zsh
+++ custom/history.zsh
@@ -1,5 +1,6 @@
 ## Command history configuration
-export HISTIGNORE="&:ls:ll:la:lrt:l:da:add:psl:hgrep:HG:[bf]g:exit:reset:clear:cd:cd -:cd ..:cd..:rm"
+export HISTIGNORE="&:ls:ll:la:lrt:l:da:psl:hgrep:HG *:[bf]g:exit:reset:clear:cd:cd -:cd ..:cd..:rm \*"
+export HISTORY_IGNORE="&:ls:ll:la:lrt:l:da:psl:hgrep:HG *:[bf]g:exit:reset:clear:cd:cd -:cd ..:cd..:rm \*"
 HISTSIZE=999999
 SAVEHIST=999999
 setopt appendhistory
